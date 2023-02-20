fab9ce1e240f543620b097a74626d12d280688d4
ProgressAce <daiyaansallie7@gmail.com>
2023-02-19 23:42:57 -0800

    Change the layout of the statements

--- simple_shell/main.c
+++ simple_shell/main.c
@@ -10,41 +10,38 @@ int main(int argc, char **argv)
 {
 	char **cmd_line = NULL;
 	char *line = NULL;
-	ssize_t chars_read;
+	ssize_t chars;
 	size_t n = 0;
 
 	while (1)
 	{
- 		/*free memory of string inside cmd_line as well with free_buffer func*/
          	free_double_buff(cmd_line);
-		free(line);
-
 		printprompt();
 
-		if (argc < 1)
-			return (-1);
 
-
-		/*check if getline failed or reached EOF or user used Ctrl + D*/	
-		chars_read = getline(&line, &n, stdin);
-		/*create getline check*/
-		if (chars_read == -1)
-			exit(1);
-		if (chars_read == 1)
-			continue;
-
+		chars = getline(&line, &n, stdin);
+		getline_check(chars);
 		cmd_line = split_string(line, " "); /*free memory*/
 
-		if (*cmd_line == NULL || cmd_line == NULL)
+		if (cmd_line == NULL || *cmd_line == NULL || **cmd_line == '\0')
 		{
 			perror("cmd line unsuccessful");
 			continue;
 		}
 
-		execute_cmd(cmd_line, argv);
+		path = find_path(cmd_line[0]);
+		permission = permision_check(path);
+
+		if (permission == -1)
+			perror(argv[0]);
+		else
+			execute_cmd(cmd_line, argv);
 
 /*		if (shell.interact != 1)
 			_exit();*/
 	}
+
+
+
 	return (0);
 }
