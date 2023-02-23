cb21bd6bf0e1b22975cea13addc5a72a921c9d82
ZiphoZZ <nkunkuzazz@gmail.com>
2023-02-22 17:24:40 -0800

    getenv

---
+++ simple_shell/getenv.c
@@ -0,0 +1,90 @@
+#include "main.h"
+
+/**
+  * get_environ - returns the string array copy of the environ
+  * @info: structure containing potential arguments.
+  * Return: 0 always
+  */
+char **get_environ(info_t *info)
+{
+	if (!info->environ = list_to_strings(info->env)
+	{
+		info->environ = lsit_to_strings(info->env);
+		info->env_changed = 0;
+	}
+
+	return (info->environ);
+}
+
+/**
+  * _unsetenv - remove an environment variable
+  * @info: structure containig potential arguments.
+  * Return: 1 on delete, 0 otherwise
+  * @var: the string env var propety
+  */
+int _unsetenv(info_t *info, char *var)
+{
+	list_t *node = info->env;
+	size_t i = 0;
+	char *p;
+
+	if (!node || !var)
+		return (0);
+
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			info->env_changed = delete_node_at_index(&(info->env), i);
+			i = 0;
+			node = info->env;
+			continue;
+		}
+		node = node->next;
+		i++;
+	}
+	return (info->env_changed);
+}
+
+/**
+  * _setenv - initialise new environment variable
+  * @info: structure containing potential arguments
+  * @var: string env variable property
+  * @value: the string env var value
+  * Return: 0 always
+  */
+int _setenv(info_t *info, char *var, char *value)
+{
+	char *buf = NULL;
+	list_t *node;
+	char *p;
+
+	if (!var || !value)
+		return (0);
+
+	buf = malloc(_strlen(var) + _strlen(value) + 2);
+	if (!buf)
+		return (1);
+	_strcpy(buf, var);
+	_strcat(buf, "=");
+	_strcat(buf, value);
+	node = info->env;
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			free(node->str);
+			node->str = buf;
+			info->env_changed = 1;
+			return (0);
+		}
+		node = node->next;
+	}
+	add_node_end(&(info->env), buf, 0);
+	free(buf);
+	info->env_changed = 1;
+	return (0);
+}
+
